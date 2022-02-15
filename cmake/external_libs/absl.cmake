aad411862615de20b537dd1395f09c974ed3783f
Damien Nguyen <damien1@huawei.com>
2022-02-15 11:23:20 +0100

    Add support for system pkgs: abseil-cpp

--- cmake/external_libs/absl.cmake
+++ cmake/external_libs/absl.cmake
@@ -13,14 +13,17 @@ endif()
 mindspore_add_pkg(
   absl
   VER 20200923.3
+  FORCE_EXACT_VERSION
   LIBS absl_strings absl_throw_delegate absl_raw_logging_internal absl_int128 absl_bad_optional_access
+  LIBS_CMAKE_NAMES strings throw_delegate raw_logging_internal int128 bad_optional_access
   URL ${REQ_URL}
   MD5 ${MD5}
   CMAKE_OPTION -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=TRUE -DCMAKE_CXX_STANDARD=11
-  TARGET_ALIAS mindspore::absl_strings absl::absl_strings
-  TARGET_ALIAS mindspore::absl_throw_delegate absl::absl_throw_delegate
-  TARGET_ALIAS mindspore::absl_raw_logging_internal absl::absl_raw_logging_internal
-  TARGET_ALIAS mindspore::absl_int128 absl::absl_int128
-  TARGET_ALIAS mindspore::absl_bad_optional_access absl::absl_bad_optional_access)
+  PATCHES ${TOP_DIR}/third_party/patch/absl/absl.patch001
+  TARGET_ALIAS mindspore::absl_strings absl::strings
+  TARGET_ALIAS mindspore::absl_throw_delegate absl::throw_delegate
+  TARGET_ALIAS mindspore::absl_raw_logging_internal absl::raw_logging_internal
+  TARGET_ALIAS mindspore::absl_int128 absl::int128
+  TARGET_ALIAS mindspore::absl_bad_optional_access absl::bad_optional_access)
 
 include_directories(${absl_INC})
